91bec6f886019c06114b99619038d99d4a7f8b32
Maxim <jidtirar@gmail.com>
2021-11-25 20:09:43 +0500

    Добавьте файлы проекта.

---
+++ OOP_laba2/OOP_laba2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Point {
+    //переменные для наследования
+protected:
+    int x;
+    int y;
+
+public:
+    //конструктор по умолчанию
+    Point() {
+        printf("\tКонструктор по умолчанию %p\n", this);
+    }
+    //конструктор c параметрами
+    Point(int _x, int _y) {
+        printf("\tКонструктор с параметрами %p\n", this);
+        x = _x;
+        y = _y;
+    }
+    //конструктор копирования
+    Point(const Point &point) {
+        printf("\tКонструктор копирования %p\n", this);
+        x = point.x;
+        y = point.y;
+    }
+    ~Point() {
+        printf("\tДеструктор %p\n", this);
+    }
+};
+
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    //статическое создание объектов
+    {
+        printf("Статическое создание объектов:\n");
+        Point a1;
+        Point a2(10, 3);
+        Point a3(a2);
+    }
+    printf("\n\n\n");
+    //динамическое создание объектов
+    {
+        printf("Динамическое создание объектов:\n");
+        Point* a1 = new Point;
+        Point* a2 = new Point(4, 22);
+        Point* a3 = new Point(*a2);
+
+        printf("Динамическое удаление объектов:\n");
+        delete a1;
+        delete a2;
+        delete a3;
+    }
+}
